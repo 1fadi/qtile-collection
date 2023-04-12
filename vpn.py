5657e65fee582cbf91279db13649f4a1ae4ff52b
1fadi <1fadi@proton.me>
2023-04-12 17:15:12 +0200

    created a widget to display vpn connection

---
+++ vpn.py
@@ -0,0 +1,32 @@
+from libqtile.widget import base
+import subprocess
+
+
+class VPN(base.ThreadPoolText):
+    """a simple widget to display vpn connection."""
+
+    defaults = [
+        (
+            "on",
+            "VPN ON",
+            "string to indicate the device is connected to VPN"
+        ),
+        (
+            "off",
+            "",
+            "string to indicate the device is not connected to VPN."
+        ),
+    ]
+
+    def __init__(self, **config) -> None:
+        base.ThreadPoolText.__init__(self, "", **config)
+        self.add_defaults(VPN.defaults)
+
+    def poll(self):
+        process = subprocess.run(
+            "ip a | grep tun0 | grep inet | wc -l",
+            capture_output=True,
+            text=True,
+            shell=True,
+        )
+        return self.on if int(process.stdout.strip()) else self.off
