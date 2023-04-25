ce6d53c1ee263f71d58a9a10cb345b9f00a5e9aa
1fadi <1fadi@proton.me>
2023-04-25 15:31:59 +0200

    import Network class.

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@ from .clickable_clock import ClickableClock
 from .vpn import VPN
 from .battery import Battery
 from .volume import Volume
+from .network import Network
 
 widget_defaults = dict(
     font="FiraCode Nerd Font Mono",
     fontsize=16,
-    padding=3,
+    padding=8,
 )
 
 __all__ = [
@@ -14,5 +15,6 @@ __all__ = [
     "ClickableClock",
     "VPN",
     "Battery",
-    "Volume"
+    "Volume",
+    "Network"
 ]
